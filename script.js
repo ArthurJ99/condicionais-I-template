590ef2047ecede585c1b9308da42bf0049e46f77
Arthur Jordi Alves Silva <114433340+ArthurJ99@users.noreply.github.com>
2022-11-09 21:06:11 -0300

    prática guiada de condicionais I

---
+++ script.js
@@ -0,0 +1,46 @@
+const booleano1= false
+const booleano2 = true
+
+/*if(booleano1){
+    alert('entrou no if')
+}else{
+    alert('entrou no else, a variável é false')
+}*/
+
+const booleano3 = true
+
+/* if (booleano1 === booleano2){
+    alert('O valor das variáveis 1 e 2 são iguais')
+}else if(booleano2 === booleano3){
+    alert('O valor das variáveis 2 e 3 são iguais')
+}else if (booleano1 === booleano3){
+    alert('Ovalor das variáveis 1 e 3 são iguais')
+} else {
+    alert('Não existem valores iguais')
+} */
+
+
+let idade = 10
+
+/* if (idade >= 18){
+    console.log('Tem direito ao título de eleitor')
+} else if(idade < 18){
+    console.log('Você não tem idade mínima para a emissão do título de eleitor')
+}else{
+    console.log('Voto facultativo')
+} */
+
+
+//exercicio 
+
+const media= 6
+
+if (media>=5 && media<= 10){
+    console.log('Você foi aprovado(a)!')
+}else if(media>=3 && media<5){
+    console.log('Recuperação')
+}else if(media >= 0 && media<3){
+    console.log('Você foi reprovado(a)')
+}else{
+    console.log('Dado Inválido')
+}
